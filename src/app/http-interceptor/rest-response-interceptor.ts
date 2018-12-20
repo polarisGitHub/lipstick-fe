d9a1a980314386ede4ca9ff92d6884798791b162
polaris <polaris1478@gmail.com>
2018-12-20 23:16:56 +0800

    rest

--- src/app/http-interceptor/rest-response-interceptor.ts
+++ src/app/http-interceptor/rest-response-interceptor.ts
@@ -2,16 +2,30 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {map, mergeMap} from 'rxjs/operators';
+import {ToastService} from '../tabs/service/toast.service';
+import {RestResponse} from './data/rest-response';
 
 @Injectable()
 export class HttpRestResponseInterceptor implements HttpInterceptor {
+
+    private SUCCESS = 'success';
+
+    constructor(private toastService: ToastService) {
+
+    }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // TODO error handle
         return next.handle(request).pipe(
             map((event: HttpEvent<any>) => {
                 if (event instanceof HttpResponse) {
-                    return event.clone({body: event.body['data']});
+                    const restResponse: RestResponse = Object.assign(new RestResponse(), event.body);
+                    if (restResponse.code === this.SUCCESS) {
+                        return event.clone({body: restResponse.data});
+                    } else {
+                        this.toastService.presentTopToast(restResponse.errorCode + '：' + restResponse.message, {});
+                        return event.clone({body: null});
+                    }
                 }
                 return event;
             })
