d5dcc6d28be61c72d76c23890a5ffbda4f064fe5
polaris <polaris1478@gmail.com>
2018-12-20 23:16:28 +0800

    category

--- src/app/tabs/service/lipstick.service.ts
+++ src/app/tabs/service/lipstick.service.ts
@@ -2,16 +2,11 @@ import {Injectable} from '@angular/core';
 import {Brand} from '../data/brand';
 import {Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class LipstickService {
-
-    brands: Brand[] = [
-        {id: 1, code: 'ysl', name: 'ysl'}
-    ];
 
     constructor(private httpClient: HttpClient) {
     }
