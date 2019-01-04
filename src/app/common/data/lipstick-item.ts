cfbceadeca9428f8b20ff457c1a525e27414c582
polaris <polaris1478@gmail.com>
2019-01-04 23:52:55 +0800

    result page and mock

---
+++ src/app/common/data/lipstick-item.ts
@@ -0,0 +1,10 @@
+export class LipstickItem {
+    brandCode: string;
+    brandName: string;
+    categoryCode: string;
+    categoryName: string;
+    goodsName: string;
+    skuName: string;
+    colorNo: string;
+    color: string;
+}
--- src/app/tabs/service/lipstick.service.ts
+++ src/app/tabs/service/lipstick.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
 import {Brand} from '../../common/data/brand';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {Category} from '../../common/data/category';
+import {LipstickItem} from '../../common/data/lipstick-item';
 
 @Injectable({
     providedIn: 'root'
 })
 export class LipstickService {
+
 
     constructor(private httpClient: HttpClient) {
     }
@@ -22,7 +24,15 @@ export class LipstickService {
         return this.httpClient.get<Category[]>(`/api/lipstick/product/categories/${brands_str}`);
     }
 
-    search(brands: string, categoryise: string, color: string) {
-
+    search(brands: string, categoryise: string, color: string): Observable<LipstickItem> {
+        const mockLipstick: LipstickItem = {
+            brandName: 'YSL',
+            categoryName: '唇膏',
+            goodsName: '圣罗兰莹亮纯魅液体唇膏',
+            skuName: '蜜桃唇语',
+            colorNo: 'N°3',
+            color: '#e46264'
+        };
+        return of(mockLipstick);
     }
 }
