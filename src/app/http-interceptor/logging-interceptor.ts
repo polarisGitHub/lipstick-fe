740241296626f870581aeb7155ac1cdc8dc829c8
polaris <polaris1478@gmail.com>
2019-02-23 09:43:52 +0800

    只打印response

--- src/app/http-interceptor/logging-interceptor.ts
+++ src/app/http-interceptor/logging-interceptor.ts
@@ -5,6 +5,10 @@ import {tap} from 'rxjs/operators';
 export class LoggingInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(tap(l => console.info(l)));
+        return next.handle(request).pipe(tap(event => {
+            if (event instanceof HttpResponse) {
+                console.info(event);
+            }
+        }));
     }
 }
